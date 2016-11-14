b334e3ea8473d2ef5dcb9c785f1d3d89856f58ab
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-11-14 16:25:56 -0500

    updated from upstream

--- dbQueries.js
+++ dbQueries.js
@@ -1,7 +1,14 @@
-var pgp = require('pg-promise')(/*options*/)
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
 var db = pgp('postgres://vsxebhuzjkklry:-zfG7Ek8uDVo1Rh7VEcyYSy0AR@ec2-23-23-224-174.compute-1.amazonaws.com:5432/d6utk5i40rffqd')
 
-var getUser() = db.one({
+var getUser = db.one({
 	text: "SELECT * FROM UserInfo"
   })
   .then(function (data) {
