68ca1081acc34ce5fb4caa882d2bb540b1d77a1e
Alexandru <alexandru.baluta@mail.utoronto.com>
2016-11-14 17:19:30 -0500

    Retrieved test data from database.

--- dbQueries.js
+++ dbQueries.js
@@ -1,4 +1,5 @@
 var pgp = require('pg-promise')(/*options*/)
+pgp.pg.defaults.ssl = true;
 var db = pgp('postgres://vsxebhuzjkklry:-zfG7Ek8uDVo1Rh7VEcyYSy0AR@ec2-23-23-224-174.compute-1.amazonaws.com:5432/d6utk5i40rffqd');
 
 module.exports = {
@@ -6,7 +7,7 @@ module.exports = {
 };
 
 function getUser(req, res, next) {
-	db.one('SELECT * FROM UserInfo')
+	db.one('SELECT * FROM appData.UserInfo')
   .then(function (data) {
     console.log('DATA:', data)
     res.status(200)
