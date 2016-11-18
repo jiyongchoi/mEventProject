57b9a8a19657dcc7fe615ba5a701cc726df13b41
Alexandru <alexandru.baluta@mail.utoronto.com>
2016-11-17 22:53:35 -0500

    Updated /src db calls

--- db/dbFunctions.sql
+++ db/dbFunctions.sql
@@ -4,10 +4,21 @@ CREATE TYPE userType AS (username varchar(25),
 	surname varchar(25)
 );
 
-CREATE FUNCTION getUser (username varchar(25), password varchar(16))
+CREATE FUNCTION getUser (username varchar(25), 
+						 password varchar(16))
 RETURNS userType
 AS 'SELECT * 
-	FROM appData.UserInfo 
-	WHERE appData.UserInfo.username = $1 
-	AND appData.UserInfo.password = $2 '
+	FROM appData.UserInfo u
+	WHERE u.username = $1 
+	AND u.password = $2'
+LANGUAGE SQL;
+
+CREATE FUNCTION postUser (username varchar(25), 
+						  password varchar(16),
+						  firstname varchar(25),
+						  surname varchar(25))
+RETURNS int
+AS 'INSERT INTO appData.UserInfo
+	VALUES ($1, $2, $3, $4);
+	SELECT 0;'
 LANGUAGE SQL;
