ce686f190edfad8d05b109429b4366d69c0077d4
Alexandru <alexandru.baluta@mail.utoronto.com>
2016-11-17 20:07:44 -0500

    Created getUser psql function.

---
+++ accessDB.js
@@ -0,0 +1,25 @@
+var pgp = require('pg-promise')(/*options*/)
+pgp.pg.defaults.ssl = true;
+var db = pgp('postgres://vsxebhuzjkklry:-zfG7Ek8uDVo1Rh7VEcyYSy0AR@ec2-23-23-224-174.compute-1.amazonaws.com:5432/d6utk5i40rffqd');
+
+module.exports = {
+  getUser: getUser
+};
+
+function getUser(req, res, next) {
+	var username = req.params.query.username;
+	var password = req.params.query.password;
+	db.one('SELECT * FROM getUser(' + username + ', ' + password + ')')
+  .then(function (data) {
+    console.log('DATA:', data)
+    res.status(200)
+    .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved user'
+    });
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  });
+}; 
