103ccad93ede1776835b2ffad06c05b6b9dd4518
Lukas Frantzke <lukas.frantzke@mail.utoronto.ca>
2016-11-26 14:46:47 -0500

    UserInfo now updates Name and Rating

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,22 +3,40 @@
 import React from 'react';
 import LoginForm from './Login';
 import SignupForm from './Signup';
-import TopNav from './TopNav';
+import UserInfo from './UserInfo';
+import EventManager from './EventManager';
 
 export default class IndexPage extends React.Component {
-  render() {
-    return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-6">
-          <LoginForm/>
-        </div>
-        <div className="col-sm-6">
-          <SignupForm/>
-        </div>
-      </div>
-    </div>
-    );
-  }
+
+	constructor() {
+		super();
+
+		this.state = {
+			user: { username : "Clayton", rating:"5"},
+			events: {
+					1 : {
+							name: "event1"
+						},
+					2 : {
+							name: "event2"
+						},
+					},
+		};
+	}
+
+	render() {
+		return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-sm-6">
+					<LoginForm/>
+				</div>
+				<div className="col-sm-6">
+					<SignupForm/>
+				</div>
+			</div>
+		</div>
+		);
+	}
 }
 
