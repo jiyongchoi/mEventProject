02f3beca6467e2330a611021684333d4138f6c5b
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-11-26 14:33:56 -0500

    added table

---
+++ src/server.js
@@ -0,0 +1,111 @@
+import Express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './routes';
+import path from 'path';
+import bodyParser from 'body-parser';
+import accessDB from './accessDB';
+
+var session = require("client-sessions");
+var uuid = require('node-uuid');
+var app = Express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(Express.static(path.join(__dirname, 'static')));
+app.use( bodyParser.json() );      
+app.use(bodyParser.urlencoded({     
+    extended: true
+}));
+
+app.use(session({
+  cookieName: 'session',
+  secret: uuid.v1()
+}))
+
+function checkAuth(req, res, next) {
+  if (!req.session.username) {
+    res.send('You are not authorized to view this page');
+  } else {
+    next();
+  }
+}
+
+app.get('/', function(req, res) {
+	match({routes, location: req.url},
+		function (err, renderProps) {
+			if (err) {
+				return res.status(500).send(err.message);
+			}
+			let markup;
+			if (renderProps) {
+				markup = renderToString(<RouterContext {...renderProps}/>);
+			}
+			return res.render('index', { markup });
+		});
+});
+
+ app.post('/userlogin', accessDB.verifyUser); // back-end DB route
+
+ app.post('/usersignup', accessDB.postUser); // back-end DB route
+
+ app.get('/userpage/:id', checkAuth, function(req, res) {
+ 	match({routes, location: req.url},
+		function (err, renderProps) {
+			if (err) {
+				return res.status(500).send(err.message);
+			}
+			let markup;
+			if (renderProps) {
+				markup = renderToString(<RouterContext {...renderProps}/>);
+			}
+			return res.render('index', { markup });
+		});
+ });
+
+app.get('/mainpage/:id', checkAuth, function(req, res) {
+ 	match({routes, location: req.url},
+		function (err, renderProps) {
+			if (err) {
+				return res.status(500).send(err.message);
+			}
+			let markup;
+			if (renderProps) {
+				markup = renderToString(<RouterContext {...renderProps}/>);
+			}
+			return res.render('index', { markup });
+		});
+ });
+
+app.get('/eventpage/:id',  checkAuth, function(req, res) {
+ 	match({routes, location: req.url},
+		function (err, renderProps) {
+			if (err) {
+				return res.status(500).send(err.message);
+			}
+			let markup;
+			if (renderProps) {
+				markup = renderToString(<RouterContext {...renderProps}/>);
+			}
+			return res.render('index', { markup });
+		});
+ });
+
+/*function nocache(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}*/
+
+app.get('/logout', checkAuth, function (req, res) {
+	req.session.reset();
+	res.send({redirect:"/"});
+}); 
+
+app.post('/user', accessDB.postUser);
+//app.delete('/user', routes.deleteUser);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
