23fbe197c05d2aada20911dfaa5ae6f5700e2ae9
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-12-02 15:26:39 -0500

    got rid of problem in eventpage

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -5,14 +5,6 @@ export default class EventPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {event:{}};
-		this.getEvent = this.getEvents.bind(this);
-		//Call function to get user for given username
-		this.getEvent();
-	}
-
-	getEvent() {
-		super(props);
-		
 	}
 	//AJAX CALL call get('/events?type=rating')
 	render(){
