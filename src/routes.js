45a23a1e35c1cbead5f50e5736fff0de1e5bbb3c
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-12-02 13:51:33 -0500

    added userpage to routes

--- src/routes.js
+++ src/routes.js
@@ -12,101 +12,13 @@ import AddEventPage from './components/AddEventPage';
 import AdminPage from './components/AdminPage';
 
 import axios from 'axios';
-// import localStorage from 'react-localstorage';
-
-
-// function alreadyAuthen(nextState, replace, cb) {
-// 	// for already logged in state
-// 	let current_session = '';
-// 	axios.get("/current_session")
-// 		.then(function(response) {
-// 			current_session = response.data;	
-// 		})
-// 		.catch(function(error) {
-// 			console.log(error);
-// 		});
-// 	if (current_session.localeCompare("") != 0) {
-// 		replace({
-//       		pathname: '/mainpage' + current_session,
-//       		state: { nextPathname: nextState.location.pathname }
-//     	});
-// 	}
-
-// 	cb();
-
-// }
-
-function loggedOut(nextState, replace, cb) {
-	let current_session = '';
-	axios.get("/current_session")
-		.then(function(response) {
-			current_session = response.data;	
-		}.bind(this))
-		.catch(function(error) {
-			console.log(error);
-		}.bind(this));
-	console.log("SESSION: " + current_session);
-	// if (current_session.localeCompare("") == 0) {
-	// 	replace({
- //      		pathname: '/',
- //      		state: { nextPathname: nextState.location.pathname }
-	// 	})
-	// }
-	var tok = JSON.parse(localStorage.getItem('token'));
-	console.log("TOKEN: " + tok);
-	if (JSON.parse(localStorage.getItem("token"))) {
-		replace({
-      		pathname: '/',
-      		state: { nextPathname: nextState.location.pathname }
-		});
-	}
-
-	cb();
-}
-
-function alreadyAuthen(nextState, replace, cb) {
-	let current_session = '';
-	axios.get("/current_session")
-		.then(function(response) {
-			current_session = response.data;	
-		}.bind(this))
-		.catch(function(error) {
-			console.log(error);
-		}.bind(this));
-	console.log("asdfasdf");
-	console.log("session: " + current_session);
-	var tok = JSON.parse(localStorage.getItem('token'));
-	console.log("token: " + tok);
-	if (JSON.parse(localStorage.getItem("token"))) {
-		replace({
-      		pathname: '/mainpage/' + current_session,
-      		state: { nextPathname: nextState.location.pathname }
-    	});
-	}
-	cb();
-}
-
-// function loggedOut(nextState, replace, cb) {
-// 	// if (typeof localStorage.token == "undefined") {
-// 	// 	replace({
-//  //      		pathname: '/',
-//  //      		state: { nextPathname: nextState.location.pathname }
-// 	// 	})
-// 	// }
-// 	if (localStorage.token) {
-// 		replace({
-//       		pathname: '/mainpage',
-//       		state: { nextPathname: nextState.location.pathname }
-//     	});
-// 	}
-// 	cb();
-// }
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={IndexPage} />
     <Route path="mainpage/:id" component={MainPage}/>
-    <Route path="eventpage/:id" component={EventPage}/>
+    <Route path="userpage/:id" component={UserPage}/>
+    <Route path="eventpage/:id/:eventid" component={EventPage}/>
     <Route path="addeventpage" component={AddEventPage}/>
     <Route path="admin" component={AdminPage}/>
   </Route>
