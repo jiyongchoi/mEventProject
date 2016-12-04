b69b200063fd885ee5b6af05e29e3f0bb9619f6b
Alexandru <alexandru.baluta@mail.utoronto.ca>
2016-12-04 16:39:08 -0500

    Got rid of unneccessary line.

--- src/accessDB.js
+++ src/accessDB.js
@@ -1,7 +1,6 @@
 var pgp = require('pg-promise')(/*options*/)
 pgp.pg.defaults.ssl = true;
 var db = pgp('postgres://vsxebhuzjkklry:-zfG7Ek8uDVo1Rh7VEcyYSy0AR@ec2-23-23-224-174.compute-1.amazonaws.com:5432/d6utk5i40rffqd');
-var async = require('async');
 /*
 * FUNCTIONS FOR USERS
 */
