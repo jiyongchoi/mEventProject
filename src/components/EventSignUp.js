d63837085559db9841c57ada5e018a5753ffa819
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-12-04 17:32:24 -0500

    added comment to eventsignup

--- src/components/EventSignUp.js
+++ src/components/EventSignUp.js
@@ -3,12 +3,16 @@
 import React from 'react';
 import axios from 'axios';
 
+/*
+* Express the mechanism to sign up the current logged in user to the eventid event
+*/
 export default class EventSignUp extends React.Component {
 	constructor(props) {
 		super(props);	
 		this.signupevent = this.signupevent.bind(this);
 	}
 
+	// Make ajax call to the server to sign up the currently logged in user
 	signupevent(event) {
 		axios.post('/eventattendees', {eventid: this.props.eventid})
 			.then(function(response){
@@ -21,10 +25,12 @@ export default class EventSignUp extends React.Component {
 			}.bind(this));
 	}
 
+	// Express the button that, on click, will trigger the ajax call to submit user
 	render() {
 		return (
 			<div className="col-sm-2">
-				<button className="btn btn-primary btn-lg" onClick={this.signupevent}>SIGN UP FOR THIS EVENT</button>
+				<button className="btn btn-primary btn-lg" 
+					onClick={this.signupevent}>SIGN UP FOR THIS EVENT</button>
 			</div>
 
 		)
