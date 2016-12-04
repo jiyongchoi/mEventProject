70bc323f669b22de48a4362d0cc10cfbe4284869
jiyongchoi <jiyong.choi.gthb@gmail.com>
2016-12-04 17:00:56 -0500

    fixed message in writereview

--- src/accessDB.js
+++ src/accessDB.js
@@ -1,7 +1,6 @@
 var pgp = require('pg-promise')(/*options*/)
 pgp.pg.defaults.ssl = true;
 var db = pgp('postgres://vsxebhuzjkklry:-zfG7Ek8uDVo1Rh7VEcyYSy0AR@ec2-23-23-224-174.compute-1.amazonaws.com:5432/d6utk5i40rffqd');
-var async = require('async');
 /*
 * FUNCTIONS FOR USERS
 */
@@ -358,7 +357,7 @@ exports.addReview = function(req, res, next) {
             });
     }
     else {
-      return res.status(200).send("You are not on the attendee list");
+      return res.status(200).send("You are not on the attendee list or event has not happened yet");
     }
     
 }
