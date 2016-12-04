c3fcf12b88fb7fb601d203dc912eda76e8df95e9
Lukas Frantzke <lukas.frantzke@mail.utoronto.ca>
2016-12-03 21:51:39 -0500

    Added documentation to MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import UserInfo from './UserInfo.js';
 import EventManager from './EventManager.js';
-import Actions from './Actions';
 import axios from 'axios';
 import MainTopNav from './MainTopNav';
 
 export default class MainPage extends React.Component {
-// componentdidmount (ajax call to get all events)
 	constructor() {
 		super();
 		this.state = { userID: "" };
 		this.getLoggedInUser = this.getLoggedInUser.bind(this);
+		//Gets the currently logged in User
 		this.getLoggedInUser();
 	}
 
 	getLoggedInUser(){
+		//Gets currently logged in user from server
 		axios.get('/current_session')
 		      	.then(function(response) {
 		      		console.log("CURRENT SESSION USER:"+JSON.stringify(response.data));
-		      		//Sets the user info to the logged in user
+		      		//Sets the user to the logged in user
 		      		this.setState({ userID: response.data});
-		      		//this.getUserInfo(response.data);
 		      	}.bind(this))
 		      	.catch(function (error) {
     				console.log(error.message);
@@ -28,11 +27,15 @@ export default class MainPage extends React.Component {
 	}
 
 	render() {
-		//Gets URL paramater as username to load userinfo 
+		//Check if user has been loaded from the server
 		let userIsLoggedIn = null;
 		if(this.state.userID.localeCompare("") == 0){
+			//If call to server is not yet complete.
+			//Show user loading
 			userIsLoggedIn = <div>Loading User</div>;
 		} else {
+			//If username has been set from the server
+			//Load the UserInfo
 			userIsLoggedIn = <UserInfo username={this.state}/>
 		}
 		return (
